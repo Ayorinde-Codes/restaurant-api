0040bf5c3b96151ddd9e82a77c675a66a1f0bc4c
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2022-07-24 23:41:55 +0100

    add data to restaurant

---
+++ database/migrations/2022_07_24_220034_add_restaurant_to_restaurant_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddRestaurantToRestaurantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('restaurants')->insert([
+            [
+                'name' => 'Burger king',
+                'contact' => '111111',
+                'address' => 'Noida sector 50',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Domino',
+                'contact' => '111111',
+                'address' => 'Noida sector 50',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
